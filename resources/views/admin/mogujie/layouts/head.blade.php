b30a04bfc2301e1ba6340c307d4ea68634155150
zmg <z20200602@outlook.com>
2022-03-02 14:09:40 +0800

    admin

--- resources/views/admin/mogujie/layouts/head.blade.php
+++ resources/views/admin/mogujie/layouts/head.blade.php
@@ -3,11 +3,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-  <title>用户列表</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title> {{$title}} </title>
   <link rel="icon" href="/img/mogujie/admin/favicon.ico" type="image/ico">
   <meta name="keywords" content="LightYear,光年,后台模板,后台管理系统,光年HTML模板">
   <meta name="description" content="LightYear是一个基于Bootstrap v3.3.7的后台管理系统的HTML模板。">
-  <meta name="author" content="yinqi">
+  <meta name="author" content="zmg">
   <link href="/css/mogujie/admin/bootstrap.min.css" rel="stylesheet">
   <link href="/css/mogujie/admin/materialdesignicons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="/js/mogujie/admin/jconfirm/jquery-confirm.min.css">
