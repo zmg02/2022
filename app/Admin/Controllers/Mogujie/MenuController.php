81ee95130d5d620e1393f413cb8cc478080fcd46
zmg <1096173392@qq.com>
2022-03-05 17:02:58 +0800

    menu

--- app/Admin/Controllers/Mogujie/MenuController.php
+++ app/Admin/Controllers/Mogujie/MenuController.php
@@ -12,53 +12,13 @@ class MenuController extends Controller
     public function index(Request $request)
     {
         $menuM = new AdminMenu();
+        $menus = $menuM->orderBy('order', 'DESC')->get()->toArray();
+        $tree = $this->treeArray($this->getTreeLevel($menus));
+        $menusHtml = $this->getMenuSelect($tree);
+        $menusArr  = $this->getMenulists($tree);
 
-        if (isset($request['submit'])) {
-            $menuM->validate($request);
-            $result = $menuM->addMenu($request);
-            if ($result) {
-                return redirect(route('menu'));
-            }
-        } else {
-
-            $menus = $menuM->orderBy('order', 'DESC')->get()->toArray();
-            $tree = $this->treeArray($this->getTreeLevel($menus));
-            $menusHtml = $this->getMenuSelect($tree);
-
-            $menusArr  = $this->getMenulists($tree);
-
-            $icons = json_decode(file_get_contents('upload/icons.json'), true);
-            return view('admin\mogujie\menu\index', compact('menusArr', 'menusHtml', 'icons'));
-        }
-    }
-
-    public function getMenuInfo($id)
-    {
-        $menu = AdminMenu::withoutGlobalScope('status')->where('id', $id)->first();
-
-        $menuInfo = $menu->toArray();
-        $pTitle = AdminMenu::withoutGlobalScope('status')->where('id', $menuInfo['parent_id'])->value('title');
-        $menuInfo['parent_title'] = $pTitle ?? 'Root';
-
-        return $menuInfo;
-    }
-
-    public function destroy($id)
-    {
-        $menuM = new AdminMenu();
-        $idString = $menuM->getChildernId($id);
-        $idArray = explode(',', $idString);
-        array_pop($idArray);
-
-        if ($idArray) {
-            $res = AdminMenu::withoutGlobalScope('status')->whereIn('id', $idArray)->update(['status' => 2]);
-            if ($res) {
-                return 'success';
-            } else {
-                return ['code' => 0, 'message' => 'error'];
-            }
-        }
-        return ['code' => 0, 'message' => 'error'];
+        $icons = json_decode(file_get_contents('upload/icons.json'), true);
+        return view('admin\mogujie\menu\index', compact('menusArr', 'menusHtml', 'icons'));
     }
 
     public function create()
@@ -104,14 +64,14 @@ class MenuController extends Controller
         $pTitle = $menuM->withoutGlobalScope('status')->where('id', $menuInfo['parent_id'])->value('title');
         $menuInfo['parent_title'] = $pTitle ?? 'Root';
 
-        return view('admin\mogujie\menu\edit', compact('menusSelect', 'icons','menuInfo'));
+        return view('admin\mogujie\menu\edit', compact('menusSelect', 'icons', 'menuInfo'));
     }
 
     public function update(Request $request, $id)
     {
         $menuModel = new AdminMenu();
         $menuModel->validate($request);
-        $result = $menuModel->updateMenu($request,$id);
+        $result = $menuModel->updateMenu($request, $id);
         if ($result) {
             $message = '修改成功！';
         } else {
@@ -125,19 +85,37 @@ class MenuController extends Controller
         ]);
     }
 
+    public function destroy($id)
+    {
+        $menuM = new AdminMenu();
+        $idString = $menuM->getChildernId($id);
+        $idArray = explode(',', $idString);
+        array_pop($idArray);
+
+        if ($idArray) {
+            $res = AdminMenu::withoutGlobalScope('status')->whereIn('id', $idArray)->update(['status' => 2]);
+            if ($res) {
+                return 'success';
+            } else {
+                return ['code' => 0, 'message' => 'error'];
+            }
+        }
+        return ['code' => 0, 'message' => 'error'];
+    }
+
     public function icon($name)
     {
         $search = [];
         $iconsArray = json_decode(file_get_contents('upload/icons.json'), true);
         foreach ($iconsArray as $icon) {
             if (strstr($icon, $name)) {
-                $search[] = $icon; 
+                $search[] = $icon;
             }
         }
         if (trim($name) == 'none_search') {
             $search = $iconsArray;
         }
-        
+
         return $search;
     }
 }
